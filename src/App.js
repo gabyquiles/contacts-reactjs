f9cf09465f12e715bb9be6f787fc0abbd5b17da3
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-04-26 12:15:47 -0400

    Initial Commit for Contacts in-class project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 
-class ContactList extends Component {
+const contacts = [
+    {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+];
+
+class App extends Component {
     render() {
-        const people = this.props.contacts;
-
-        return <ol>
-            {people.map((person) => (
-                    <li>{person.name}</li>
-                )
-            )}
-        </ol>
-
+        return (
+            <div>
+                Hello World
+            </div>
+        )
     }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-            {name: 'Tyler'},
-            {name: 'Karen'},
-            {name: 'Richard'}
-        ]}/>
-        <ContactList contacts={[
-            {name: 'Amanda'},
-            {name: 'Mikenzi'},
-            {name: 'Ryan'}
-        ]}/>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default App
