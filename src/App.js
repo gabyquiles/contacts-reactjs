2613a37089f988b748d4c3cdbe34b9de3e7d87a1
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-04-30 22:32:34 -0400

    Use Route to navigate between screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
+import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI';
 
 
@@ -30,9 +32,19 @@ class App extends Component {
     render() {
         return (
             <div>
+                <Route
+                    exact
+                    path="/"
+                    render={() => (
                 <ListContacts
                     contacts={this.state.contacts}
                     onDeleteContact={this.removeContact}
+                />
+                    )}
+                    />
+                <Route
+                    path="/create"
+                    component={CreateContact}
                 />
             </div>
         )
